e20afefe592d8697b6e4641829d69b95dc7b656e
alwan mortada <almortada88@gmail.com>
2020-11-06 15:53:20 -0600

    created index action for posts controller

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
-    it 'cna be reached successfully' do
+    it 'can be reached successfully' do
         visit posts_path
         expect(page.status_code).to eq(200)
+    end
+
+    it 'has a title of Posts ' do
+      visit posts_path
+      expect(page).to have_content(/Posts/)
     end
   end
 end
